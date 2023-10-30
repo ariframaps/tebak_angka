c57e6577ca0e47525e6ac804909ed1b5036a7d31
ariframaps <ariframa@student.ub.ac.id>
2023-10-30 16:16:12 +0700

    menambahkan event listener pada tombol2 yang ada, beserta fungsi-fungsi yang dibutuhkan

--- tebak_angka/script.js
+++ tebak_angka/script.js
@@ -59,3 +59,79 @@ window.addEventListener('load', () => {
     localTotalVictoryField.innerText = localStorage.getItem(localTotalVictoryKey);
     localMaximumAttemptField.innerText = localStorage.getItem(localMaximumAttemptsKey);
 });
+
+playButton.addEventListener('click', () => {
+    sessionStorage.setItem(sessionAnswerKey, getAnswer());
+    sessionStorage.setItem(sessionUserIsPlayingKey, true);
+    duringGameDisplay.removeAttribute('hidden');
+    beforeGameDisplay.setAttribute('hidden', false);
+});
+
+answerButton1.addEventListener('click', () => {
+    sessionUserAnswerField.innerText += '1';
+    if (sessionUserAnswerField.innerText.length == 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+answerButton2.addEventListener('click', () => {
+    sessionUserAnswerField.innerText += '2';
+    if (sessionUserAnswerField.innerText.length == 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+answerButton3.addEventListener('click', () => {
+    sessionUserAnswerField.innerText += '3';
+    if (sessionUserAnswerField.innerText.length == 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+function checkAnswer(userAnswer) {
+    console.log(sessionStorage.getItem(sessionAnswerKey));
+    console.log('mengecek jawaban');
+    if (sessionStorage.getItem(sessionAnswerKey) == userAnswer) {
+        console.log('jawaban benar');
+        afterGameDisplay.removeAttribute('hidden');
+        duringGameDisplay.setAttribute('hidden', true);
+        sessionTrueAnswerField.innerText = userAnswer;
+        updateScore();
+    } else {
+        sessionUserWrongAnswerField.innerText = userAnswer;
+        const previousAttempt = parseInt(sessionStorage.getItem(sessionUserAttemptsKey));
+        sessionStorage.setItem(sessionUserAttemptsKey, previousAttempt + 1);
+        sessionUserAttemptsAmountField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
+        sessionUserAnswerField.innerText = '';
+    }
+}
+
+function updateScore() {
+    const sessionAttemptValue = parseInt(sessionStorage.getItem(sessionUserAttemptsKey));
+    const localAttemptValue = parseInt(localStorage.getItem(localMaximumAttemptsKey));
+
+    if (sessionAttemptValue > localAttemptValue) {
+        localStorage.setItem(localMaximumAttemptsKey, sessionAttemptValue);
+        localMaximumAttemptField.innerText = localStorage.getItem(localMaximumAttemptsKey);
+    }
+
+    const previousTotalVictoryAmount = parseInt(localStorage.getItem(localTotalVictoryKey));
+    localStorage.setItem(localTotalVictoryKey, previousTotalVictoryAmount + 1);
+    localTotalVictoryField.innerText = localStorage.getItem(localTotalVictoryKey);
+}
+
+window.addEventListener('beforeunload', function () {
+    sessionUserAnswerField.innerText = '';
+    sessionUserWrongAnswerField.innerText = '';
+    sessionStorage.setItem(sessionUserAttemptsKey, 0);
+    sessionUserAttemptsAmountField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
+});
+
+destroyDataButton.addEventListener('click', function () {
+    sessionStorage.removeItem(sessionAnswerKey);
+    sessionStorage.removeItem(sessionUserAttemptsKey);
+    sessionStorage.removeItem(sessionUserIsPlayingKey);
+    localStorage.removeItem(localTotalVictoryKey);
+    localStorage.removeItem(localMaximumAttemptsKey);
+    alert('Mohon me-refresh halaman ini kembali');
+});
